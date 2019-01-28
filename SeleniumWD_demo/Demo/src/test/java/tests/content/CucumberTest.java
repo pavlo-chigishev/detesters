a63460f83fc554a5f36d83962559953bab828e0a
Pavlo Chigishev <hello@pavlo.nl>
2019-01-28 16:24:05 +0100

    added commentary in the cucumber test

--- SeleniumWD_demo/Demo/src/test/java/tests/content/CucumberTest.java
+++ SeleniumWD_demo/Demo/src/test/java/tests/content/CucumberTest.java
@@ -8,5 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features="src/test/Features",glue={"src/test/java/tests/StepDefinition"},plugin = {"pretty"})
 public class CucumberTest
 {
-
+    // do not code here anything,
+    // all gonna be coded in the steps definitions
 }
